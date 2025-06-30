8902d059e61f932935336cbe8c1635c4603234b5
ABabayaga <alef_rdl@hotmail.com>
2025-06-30 13:06:34 -0400

    Add do texto sobre

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -19,12 +19,15 @@ export default function ServicesSection() {
 
           {/* Coluna do texto (agora à direita) */}
           <div className="col-md-8">
-            <h2 className="text-start">Welcome</h2>
-            <p className={`text-justify ${styles.text1}`}>
-              Eum a debeabst and trency carical cose hokcost based ni Sociy tal.
-              Brebase, I am committed to helping individuals navigate Sfo's challenges and cultivate resilience. I have extensive experience working with people
-              facing psychological challenges in both hospital and community
-              settings.<br/> I keep a compassionate tone while addressing a range of concerns during clinical sessions, and my therapeutic approach is an integrative form of psychotherapy using the most current evidence-based techniques.
+            <h2 className={`text-start ${styles.title1}`}>Bem-vindo(a)</h2>
+            <p className={`text-justify ${styles.text}`}>
+              Olá, sou Pãmella da Silva Aranda, psicóloga com atuação fundamentada na Terapia Cognitivo-Comportamental (TCC) e, atualmente, dedicada ao aprofundamento na Terapia de Aceitação e Compromisso (ACT), uma abordagem que tem ampliado minha escuta clínica e minha prática profissional.
+
+              Sou mestre e doutoranda em Psicologia da Saúde, com foco em cuidados paliativos – tema que me mobiliza profundamente por seu olhar humano e integrativo sobre o sofrimento, a vida e o cuidado. Atuo também como professora universitária, o que me permite compartilhar e construir saberes com novos profissionais da psicologia.
+
+              Minha trajetória inclui experiências no contexto hospitalar e em serviços de saúde mental como o CAPS, onde acompanhei pessoas com transtornos mentais graves e severos. Ao longo dessa caminhada, também participei de uma residência multiprofissional em cuidados continuados integrados, o que enriqueceu minha compreensão sobre o cuidado interdisciplinar.
+
+              Acredito na psicologia como um espaço de escuta, transformação e presença. Seja muito bem-vindo(a)!
             </p>
           </div>
         </div>
