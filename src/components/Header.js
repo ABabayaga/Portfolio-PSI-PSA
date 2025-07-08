8d5f26f985254a50a57db832db92fc4adc8622b7
ABabayaga <alef_rdl@hotmail.com>
2025-07-08 13:59:19 -0400

    add button whatsapp lateral direita e no nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,11 +11,31 @@ export default function Header() {
 
       {/* Coluna central com navegação */}
       <nav className={` ${styles.title1} flex-grow-1 d-flex justify-content-center`}>
-      <a href="#home" className="mx-3">Início</a>
+        <a href="#home" className="mx-3">Início</a>
         <a href="#about" className="mx-3">Sobre</a>
         <a href="#services" className="mx-3">Serviços</a>
         <a href="#contato" className="mx-3">Contato</a>
+        <a
+          href='https://wa.me/5567996333839?text=Olá%21%20Gostaria%20de%20mais%20informações%20sobre%20o%20atendimento.'
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className={styles.contactBtn}>
+            Whatsapp
+            <img
+              src='/whatsapp.svg'
+              style={{
+                width: '50%',
+                maxWidth: '20px',
+                height: 'auto',
+                marginLeft: '5px'
+              }}
+            />
+          </button>
+        </a>
       </nav>
+
+      
 
       {/* Coluna direita "vazia" com mesma largura do logo */}
       <div style={{ width: '150px' }}></div>
--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -3,7 +3,7 @@ import styles from '@/styles/HomeSection.module.css';
 export default function HeroSection() {
   return (
     <section id='home' className={styles["hero-section"]}
-    style={{ scrollMarginTop: '100px' }}>
+      style={{ scrollMarginTop: '100px' }}>
 
       <div className={`container ${styles["hero-content"]}`}>
         <div className="row align-items-center">
@@ -26,6 +26,21 @@ export default function HeroSection() {
             />
           </div>
         </div>
+
+        <div className={styles.whatsappContainer}>
+          <a
+            href="https://wa.me/5567996333839?text=Olá%21%20Gostaria%20de%20mais%20informações%20sobre%20o%20atendimento." // ou seu link personalizado
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/whatsapp4.svg" // Certifique-se de que o ícone está em /public
+              alt="WhatsApp"
+              className={styles.whatsappIcon}
+            />
+          </a>
+        </div>
+
       </div>
     </section>
   );
