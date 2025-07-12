a1e7d104026dc1e6002d451944210322707bb1f8
ABabayaga <alef_rdl@hotmail.com>
2025-07-12 15:30:56 -0400

    Ajuste footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,32 +4,32 @@ export default function Footer() {
   return (
     <footer id='contato' className={`${styles.footer} py-5 text-white`}>
       <div className="container" style={{ maxWidth: '960px' }} >
-        <div className={`row ${styles.row}`}>
+        <div className={`${styles.row}`}>
 
           {/* Coluna esquerda */}
           <div className={styles["custom-colleft"]}>
             {/*<h2 className={styles.title}>Atendimento:</h2><p>Psicoterapia individual</p>*/}
 
             <p>
-              <span style={{ fontWeight: 'bold' }}>ATENDIMENTO:</span> Psicoterapia individual.
+              <span style={{ fontWeight: 'bold', fontFamily: 'Playfair Display, serif' }}>ATENDIMENTO:</span> Psicoterapia individual.
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>PÚBLICO:</span> Adolescentes, adultos e idosos.
+              <span style={{ fontWeight: 'bold', fontFamily: 'Playfair Display, serif' }}>PÚBLICO:</span> Adolescentes, adultos e idosos.
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>DESTINADO A:</span> Pessoas que falam português e residem no Brasil ou no exterior.
+              <span style={{ fontWeight: 'bold', fontFamily: 'Playfair Display, serif' }}>DESTINADO A:</span> Pessoas que falam português e residem no Brasil ou no exterior.
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>MODALIDADE:</span> Online e presencial em Campo Grande - MS.
+              <span style={{ fontWeight: 'bold', fontFamily: 'Playfair Display, serif' }}>MODALIDADE:</span> Online e presencial em Campo Grande - MS.
             </p>
 
             {/*<p className={styles.text} >Psicoterapia voltada ao público adulto</p>*/}
-            <div style={{ textAlign: 'center' }}>
+            {<div style={{ textAlign: 'center' }}>
               <a href='https://wa.me/5567996333839?text=Olá%21%20Gostaria%20de%20mais%20informações%20sobre%20o%20atendimento.' target="_blank"
                 rel="noopener noreferrer">
                 <button re className={styles.contactBtn}>Whatsapp<img src='/whatsapp.svg' style={{ width: '50%', maxWidth: '20px', height: 'auto', marginLeft: '8px' }} /></button>
               </a>
-            </div>
+            </div>}
           </div>
 
           {/* Coluna direita */}
@@ -37,12 +37,12 @@ export default function Footer() {
             {/*<h2 className={styles.title}>Contato</h2>*/}
 
             <p>
-              <span style={{ fontWeight: 'bold' }}>CONTATO</span>
+              <span style={{ fontWeight: 'bold', fontFamily: 'Playfair Display, serif' }}>CONTATO</span>
             </p>
 
             <div className={styles.text} >
               {/*<p> (67) 97541-3669</p>*/}
-              <p><span style={{ fontWeight: 'bold' }}>CRP:</span>  14/08821-8</p>
+              <p><span style={{ fontWeight: 'bold', fontFamily: 'Playfair Display, serif' }}>CRP:</span>  14/08821-8</p>
               <p>psipamellasaranda@gmail.com</p>
               <p>Rua: José Antônio, 2200 - Monte Castelo, Campo Grande - MS</p>
             </div>
