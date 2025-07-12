d2055eaf5bff8d65e6f340b1c505b0bf5a627ac7
ABabayaga <alef_rdl@hotmail.com>
2025-07-12 15:48:00 -0400

    add button Agenda header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,32 +10,17 @@ export default function Header() {
       </div>
 
       {/* Coluna central com navegação */}
-      <nav className={` ${styles.title1} flex-grow-1 d-flex justify-content-center`}>
-        <a href="#home" className="mx-3">Início</a>
-        <a href="#about" className="mx-3">Sobre</a>
-        <a href="#services" className="mx-3">Serviços</a>
-        <a href="#contato" className="mx-3">Contato</a>
-       {/*<a
-          href='https://wa.me/5567996333839?text=Olá%21%20Gostaria%20de%20mais%20informações%20sobre%20o%20atendimento.'
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className={styles.contactBtn}>
-            Whatsapp
-            <img
-              src='/whatsapp.svg'
-              style={{
-                width: '50%',
-                maxWidth: '20px',
-                height: 'auto',
-                marginLeft: '5px'
-              }}
-            />
-          </button>
-        </a>*/}
+      <nav className={` ${styles.title1} flex-grow-1 d-flex justify-content-center align-items-center`}>
+        <div className="d-flex align-items-center">
+          <a href="#home" className="mx-3">Início</a>
+          <a href="#about" className="mx-3">Sobre</a>
+          <a href="#services" className="mx-3">Serviços</a>
+          <a href="#contato" className="mx-3">Contato</a>
+          <a href="#agendar" className={`${styles.scheduleBtn} ms-3`}>Agendar Sessão</a>
+        </div>
       </nav>
 
-      
+
 
       {/* Coluna direita "vazia" com mesma largura do logo */}
       <div style={{ width: '150px' }}></div>
